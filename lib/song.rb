6910ad2a343bb3a6bfbc60ca439fc5acc532e56c
Roberto Gaitan <rgaitan.it@gmail.com>
2018-06-25 02:16:10 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,60 @@
+class Song
+  
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@genres = Array.new
+  @@artists = Array.new
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << @genre
+    @@artists << @artist
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.genres
+    genres_count = @@genres
+    genres_count = genres_count.uniq
+    genres_count.length
+    genres_count
+  end
+  
+  def self.artists
+    artists_array = Array.new
+    artists_array = @@artists.uniq
+    artists_array
+  end
+  
+  def self.genre_count
+    genre_hash = Hash.new
+    @@genres.each do |genres|
+      if genre_hash[genres] == nil
+        puts "nil"
+        genre_hash[genres] = 1
+      else
+        puts "found"
+        genre_hash[genres] += 1
+      end
+    end
+    genre_hash
+  end
+  
+  def self.artist_count
+    artist_hash = Hash.new
+    @@artists.each do |artists|
+      if artist_hash[artists] == nil
+        artist_hash[artists] = 1
+      else
+        artist_hash[artists] += 1
+      end
+    end
+    artist_hash
+  end
+  
+end
